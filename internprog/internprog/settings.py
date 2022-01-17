ca534f14734c6e185b0b48e2f4e8f232a55c627b
anmol-a1 <anmol.kamble2453@gmail.com>
2022-01-17 17:04:52 +0530

    final changes

--- internprog/internprog/settings.py
+++ internprog/internprog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 
@@ -116,12 +116,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,"static")]
 # STATICFILES_DIRS=[os.path.join(BASE_DIR,"BASE_DIR","static")]
 MEDIA_URL="/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR,'media_cdn')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
